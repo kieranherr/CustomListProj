44e23781a346ca4c258cf54cb73fad056469f58f
Kieran Herr <kieranherr@gmail.com>
2020-07-02 17:12:49 -0500

    Deleted some old methods that didnt work in the customlist class. Forgot to switch from master branch to CustomList branch.

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    class CustomList<T> : IEnumerable
+    {
+        public T[] items;
+        public CustomList()
+        {
+            items = new T[0];
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public class MyList<T>
+        {
+
+            public readonly int Count;
+          
+            public int Capacity { get; set; }
+           
+            
+            public void Add()
+            {
+
+            }
+        }
+    }
+}
---
+++ CustomListUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("CustomListUnitTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc5fb9bf-19c1-4bc1-a74a-164be3f417db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
